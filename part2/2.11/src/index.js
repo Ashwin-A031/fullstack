b2f6fa76c10f8ec690f82e1adada66544cdb25ea
Ashwin-A031 <ashwincusat123@gmail.com>
2025-02-05 14:43:45 +0530

    2.11

--- part2/2.11/src/index.js
+++ part2/2.11/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';  // optional, if you have custom styles
 import App from './App';  // importing the App component
 
 // Render the App component into the root element in the index.html file
